76c1ab21c2c23e0479c95ad52a5b2eddc58351f5
mehta.yash919@gmail.com <yash1999>
2018-10-06 02:06:16 +0530

    report model ready to take data and image upload working

--- android/app/src/main/java/com/back/vom/LoginActivity.java
+++ android/app/src/main/java/com/back/vom/LoginActivity.java
@@ -1,10 +1,9 @@
 package com.back.vom;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,20 +27,15 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-
-
-
-
-
-
     public void verifyUser() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null) {
-            startActivity(new Intent(this,MainActivity.class));
+        if (user != null) {
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
+
     public void register(View v) {
-        Intent i  = new Intent(this,RegisterActivity.class);
+        Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
         finish();
     }
