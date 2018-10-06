03957286ff247b2316bf74d158dc375baa302f5b
Nishay Madhani <nshmadhani@gmail.com>
2018-10-06 11:24:29 +0530

    Final Ap Done

--- android/app/src/main/java/com/back/vom/ReportActivity.java
+++ android/app/src/main/java/com/back/vom/ReportActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -148,7 +150,20 @@ public class ReportActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_report,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_ward ) {
+            WardActivity.ward = mReport.getWard();
+            startActivity(new Intent(this,WardActivity.class));
+        } else if(item.getItemId() == R.id.action_volunteers) {
+            VolunteersActivity.report = mReport;
+            startActivity(new Intent(this,VolunteersActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadUI() {
